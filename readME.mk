a634b2dc1165bd2af9b6b49237a5e65c07bc1ccf
lauramsfernandes <lauramsfernandes@gmail.com>
2021-01-02 14:35:53 +0100

    first commit

---
+++ readME.mk
@@ -0,0 +1,3 @@
+This repository is about my hands on experience during the `Elegant Browser Automation with Python and Selenium` course by Brendan Blair from Texas Academy.
+
+I highly recommend his course if you are interested in automation. It is a high standart course and the plus is he is the funniest guy. It's impossible to get bored. 
